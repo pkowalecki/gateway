4b3c628a181cf451d341efeae898577133d60fe9
Piotr Kowalecki <piotr.kowalecki@ug.edu.pl>
2024-10-03 14:38:41 +0200

    initial gateway

--- src/main/java/pl/kowalecki/gateway/GatewayApplication.java
+++ src/main/java/pl/kowalecki/gateway/GatewayApplication.java
@@ -2,9 +2,27 @@ package pl.kowalecki.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("public", r -> r.path("/api/v1/dietplanner/login", "/api/v1/dietplanner/register")
+                        .uri("lb://diet-planner-api"))
+                .route("diet-planner-api", r -> r.path("/api/v1/dietplanner/**")
+//                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://diet-planner-api"))
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
