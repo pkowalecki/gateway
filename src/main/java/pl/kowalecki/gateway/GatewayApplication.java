c119a86c32c02ec592ae5019fe68d7d29b002d59
Piotr Kowalecki <piotr.kowalecki@ug.edu.pl>
2024-10-10 14:31:28 +0200

    initial gateway

--- src/main/java/pl/kowalecki/gateway/GatewayApplication.java
+++ src/main/java/pl/kowalecki/gateway/GatewayApplication.java
@@ -1,36 +1,44 @@
 package pl.kowalecki.gateway;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class GatewayApplication {
-
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
-        Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
-
-        return builder.routes()
-                .route("diet-planner-api", r -> r
-                        .path("/api/v1/**")
-                        .filters(f -> {
-                            logger.info("Request passing through Gateway ");
-                            return f;
-                        })
-                        .uri("lb://diet-planner-api/"))
-                .build();
-
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("eureka-server", r ->
+                        r.path("/eureka/main")
+                                .filters(f -> f.setPath("/"))
+                                .uri("http://eureka-server:8761"))
+                .route("eureka-server-static", r ->
+                        r.path("/eureka/**")
+                                .uri("http://eureka-server:8761"))
+                .route("diet-planner-web", r ->
+                        r.path("/app/**")
+                                .uri("http://diet-planner-web:8081"))
+                .route("diet-planner-web-static", r ->
+                        r.path("/static/**")
+                                .uri("http://diet-planner-web:8081"))
+                .route("diet-planner-api", r ->
+                        r.path("/api/v1/dpa/**")
+                                .uri("lb://diet-planner-api"))
+
+                .build();
+
+    }
+
 
 }
---
+++ src/main/java/pl/kowalecki/gateway/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package pl.kowalecki.gateway;
+
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    GroupedOpenApi dietPlannerApi() {
+        return GroupedOpenApi.builder()
+                .group("diet-planner-api")
+                .pathsToMatch("/api/v1/dpa/**")
+                .addOpenApiCustomizer(openApi -> openApi.info(new Info().title("Diet Planner API")))
+                .build();
+    }
+}
