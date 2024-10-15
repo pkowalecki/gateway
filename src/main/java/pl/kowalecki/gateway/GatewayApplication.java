45dbf716ebf1e876388a3221d23ce3093751a079
Piotr Kowalecki <piotr.kowalecki@ug.edu.pl>
2024-10-15 15:12:33 +0200

    -new endpoint and initial app.properties.

--- src/main/java/pl/kowalecki/gateway/GatewayApplication.java
+++ src/main/java/pl/kowalecki/gateway/GatewayApplication.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
@@ -19,6 +21,9 @@ public class GatewayApplication {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("authorization-server", r->
+                        r.path("/**")
+                                .uri("lb://authorization-server"))
                 .route("eureka-server", r ->
                         r.path("/eureka/main")
                                 .filters(f -> f.setPath("/"))
@@ -34,8 +39,8 @@ public class GatewayApplication {
                                 .uri("http://diet-planner-web:8081"))
                 .route("diet-planner-api", r ->
                         r.path("/api/v1/dpa/**")
+                                .filters(f -> f.stripPrefix(2))
                                 .uri("lb://diet-planner-api"))
-
                 .build();
 
     }
